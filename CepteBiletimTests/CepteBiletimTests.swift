64d2787da2ced831850e38020dbfac1676162155
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2025-05-29 14:28:13 +0300

    Initial Commit

---
+++ CepteBiletimTests/CepteBiletimTests.swift
@@ -0,0 +1,17 @@
+//
+//  CepteBiletimTests.swift
+//  CepteBiletimTests
+//
+//  Created by Esna nur Yılmaz on 29.05.2025.
+//
+
+import Testing
+@testable import CepteBiletim
+
+struct CepteBiletimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
